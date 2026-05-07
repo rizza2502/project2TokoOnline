4d65443dc0db99c0d37200e4440ba8796c3aa7e0
Rizza <mauludia_maula_33rpl@student.smktelkom-mlg.sch.id>
2026-05-07 14:49:28 +0700

    fix pubspec lock conflict

--- lib/services/url.dart
+++ lib/services/url.dart
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-final String BaseUrl = "https://learn.smktelkom-mlg.sch.id/toko/api";
-final String BaseUrlTanpaApi = 'https://learn.smktelkom-mlg.sch.id/toko';
-=======
-final String BaseUrl = "https://learn.smktelkom-mlg.sch.id/toko/api";
->>>>>>> 874a133f891163f73d120b5c17adabae4cceedff
+final String BaseUrl = "https://learn.smktelkom-mlg.sch.id/api";
+final String BaseUrlTanpaAPi = "https://learn.smktelkom-mlg.sch.id";
